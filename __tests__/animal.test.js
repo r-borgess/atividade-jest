bf092bb68fadcbac7ebb408ae79baa25a8db613e
Rodrigo Borges <nrodrigob@gmail.com>
2022-09-26 22:16:58 -0300

    tests

---
+++ __tests__/animal.test.js
@@ -0,0 +1,69 @@
+const request = require('supertest');
+const app = require('../src/app');
+const animalsData = require('../src/data/animals.json');
+const fs = require('fs');
+
+describe('insercao de animais', () => {
+    afterAll(() => {
+        while(animalsData.length > 0){
+            animalsData.pop();
+        }
+        fs.writeFileSync('src/data/animals.json', JSON.stringify(animalsData));
+    });
+
+    it('cadastro de animal com sucesso', async () => {
+        const resposta = await request(app).post('/animais?nome=Spike&especie=Cachorro&idade=3');
+        expect(resposta.status).toBe(201);//sucesso
+    });
+
+    it('cadastro de animal com idade invalida', async () => {
+        const resposta = await request(app).post('/animais?nome=Mimi&especie=Gato&idade=jovem');
+        expect(resposta.status).toBe(400);//falha
+    });
+
+    it('cadastro de animal com nome curto', async () => {
+        const resposta = await request(app).post('/animais?nome=J&especie=Hamster&idade=1');
+        expect(resposta.status).toBe(400);//falha
+    });
+
+    
+});
+
+describe('retorno de animais', () => {
+
+    beforeAll(() => {
+        animalsData.push({
+            'id': 'AnimalTeste1',
+            'nome': 'Spike',
+            'especie': 'Cachorro',
+            'idade': 3
+        });
+        animalsData.push({
+            'id': 'AnimalTeste2',
+            'nome': 'Mimi',
+            'especie': 'Gato',
+            'idade': 'jovem'
+        });
+        animalsData.push({
+            'id': 'AnimalTeste3',
+            'nome': 'J',
+            'especie': 'Hamster',
+            'idade': 1
+        });
+        fs.writeFileSync('src/data/animals.json', JSON.stringify(animalsData));
+    });
+
+    afterAll(() => {
+        while(animalsData.length > 0){
+            animalsData.pop();
+        }
+        fs.writeFileSync('src/data/animals.json', JSON.stringify(animalsData));
+    });
+
+    it('retorno de lista de todos os animais cadastrados', async () => {
+        const resposta = await request(app).get('/animais');
+        expect(resposta.status).toBe(200);//sucesso
+        expect(resposta.body.length).toBe(3);//tam 3
+        //console.log(resposta.body)
+    });
+})
---
+++ src/controllers/animal-controller.js
@@ -0,0 +1,40 @@
+const fs = require('fs');
+const animalsData = require('../data/animals.json');
+const { nanoid } = require('nanoid');
+
+exports.post = (req, res, next) => {
+  // Formato da requisição: /animais?nome=Dog&especie=Cachorro&idade=4
+  // Dados recebidos
+  const nome = req.query.nome;
+  const especie = req.query.especie;
+  const idade = req.query.idade;
+
+  // Validações
+  if (isNaN(idade)) {
+    res.status(400).send({'mensagem': 'A idade do animal deve ser um número.'});
+  }
+
+  if (nome.length < 2) {//nome curto
+    res.status(400).send({'mensagem': 'O nome do animal deve ter pelo menos dois caracteres.'});
+  }
+
+  // Novo animal a ser inserido
+  const novoAnimal = {
+    'id': nanoid(),
+    'nome': nome,
+    'especie': especie,
+    'idade': idade,
+  }
+
+  // Persistência
+  animalsData.push(novoAnimal);
+  fs.writeFileSync('src/data/animals.json', JSON.stringify(animalsData));
+
+  // Envio de resposta indicando sucesso
+  res.status(201).send(novoAnimal);
+};
+
+exports.get = (req, res, next) => {
+  // Retorna todos os animais cadastrados
+  res.status(200).send(animalsData);
+};
